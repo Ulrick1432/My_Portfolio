e77521a0dbe8bae66b203a9c99ca05da35f5be42
TheaPC <ulrick.preben.hansen@live.dk>
2025-01-03 20:58:14 +0100

    custom colors added to tailwind config and added to the html elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'background': '#1A1A40',
+      'primary': '#D9D9D9',
+      'secondary': '#41469E'
+    },
     extend: {},
   },
   plugins: [],
