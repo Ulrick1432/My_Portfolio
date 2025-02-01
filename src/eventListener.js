b4aa736845fd29adcceec6d44f3d19533c55099a
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2025-02-01 19:36:12 +0100

    Maked the site more responsive and added sidebar

--- src/eventListener.js
+++ src/eventListener.js
@@ -1,3 +1,13 @@
 document.getElementById('visualize-algorithms').addEventListener('click', function() {
     window.location.href = 'https://ulrick1432.github.io/visualize_algorithms/';
 });
+
+document.querySelector('.menu-open').addEventListener('click', () => {
+    document.querySelector('.sidebar').removeAttribute('hidden');
+    document.querySelector('.menu-open').setAttribute('hidden', '');
+})
+
+document.querySelector('.menu-close').addEventListener('click', () => {
+    document.querySelector('.sidebar').setAttribute('hidden', '');
+    document.querySelector('.menu-open').removeAttribute('hidden');
+})
